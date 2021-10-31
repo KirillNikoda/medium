6b39cd643a612e8218fbc07132da2fd7f4bac79b
KirillNikoda <nikodakirill386@gmail.com>
2021-10-31 04:29:07 +0300

    feat: add IRegisterRequest & CurrentUser interfaces

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,7 +1,8 @@
 import { createAction, props } from '@ngrx/store';
+import { IRegisterRequest } from '../../types/registerRequest.interface';
 import { ActionTypes } from '../actionTypes';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{ username: string; email: string; password: string }>()
+  props<IRegisterRequest>()
 );
---
+++ src/app/shared/types/currentUser.interface.ts
@@ -0,0 +1,12 @@
+export interface ICurrentUser {
+  id: number;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  username: string;
+  bio: string | null;
+  image: string | null;
+  token: string;
+}
+
+export const HELLO_WORLD = 5;
